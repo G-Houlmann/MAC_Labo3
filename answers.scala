eeb56940dee8749f98796c41aefc1f16eca24630
Gildas <gildas.houlmann@gmail.com>
2020-12-08 17:01:26 +0100

    more answers

--- answers.scala
+++ answers.scala
@@ -8,5 +8,21 @@ val rateMin = 3.3
 val rateMax = 5.7
 rddMovies.filter(m => m.rating > rateMin && m.rating <= rateMax).sortBy(_.rating).map(m => (m.rating, m.title)).foreach(println)
 
-//EX3 - MARCHE PAS
-rddMovies.flatMap(_.genres).groupBy(m => m).map(m => (m, size(m._2))).foreach(println)
+//EX3 - TODO check les espaces ou jsais pas quoi
+val amountToShow = 5
+rddMovies.flatMap(_.genres).map(m => (m, 1)).reduceByKey((x,y) => x+y).sortBy(_._2, ascending=false).take(amountToShow).foreach(println)
+
+//Ex4 - TODO affichage
+rddMovies.map(m => (m.year, (m.votes, 1))).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).mapValues({case (v, c) => v.toFloat/c}).sortBy(_._2, ascending=false).foreach(println)
+
+
+//Part 2
+
+//WIP
+rddMovies.map(m => (m.year, (m.votes, 1))).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).mapValues({case (v, c) => v.toFloat/c}).sortBy(_._2, ascending=false).foreach(println)
+
+
+
+
+
+
