eafe836efd1d9348f180c6df6a10adc3f07ba671
Gildas <gildas.houlmann@gmail.com>
2020-12-08 21:51:13 +0100

    small adjustments

--- answers.scala
+++ answers.scala
@@ -8,22 +8,14 @@ val rateMin = 3.3
 val rateMax = 5.7
 rddMovies.filter(m => m.rating > rateMin && m.rating <= rateMax).sortBy(_.rating).map(m => (m.rating, m.title)).foreach(println)
 
-//EX3 - TODO check les espaces ou jsais pas quoi
+//EX3
 val amountToShow = 5
-rddMovies.flatMap(_.genres).map(m => (m, 1)).reduceByKey((x,y) => x+y).sortBy(_._2, ascending=false).take(amountToShow).foreach(println)
+rddMovies.flatMap(_.genres).map(m => (m.trim(), 1)).reduceByKey((x,y) => x+y).sortBy(_._2, ascending=false).take(amountToShow).foreach(println)
 
 //Ex4
 rddMovies.map(m => (m.year, (m.votes, 1))).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).mapValues({case (v, c) => v.toFloat/c}).sortBy(_._2, ascending=false).map({case (y, v) => "year: " + y + " average votes: " + v}).foreach(println)
 
 //Part 2
-
-//WIP
-val toRemove = ",;'.:!?".toSet
-rddMovies.map(m=> (m.id, m.description)).flatMapValues(_.split(" ")).mapValues(_.toLowerCase().trim().filterNot(toRemove)).take(50).foreach(println)
-
-
-
-
 def createInvertedIndex(movies: RDD[Movie]): RDD[(String, Iterable[Int])] = {
         // Define helper functions directly inside this function. In scala you can declare inner functions
         // and use them only inside the function they were declared. Useful to encapsulate/restrict 
